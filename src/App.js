04f13d68b76574e28a4bf52548a7b960e923f456
Brian <Brian@automatize.com>
2021-12-29 16:18:33 -0600

    Init

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from "./VaderLogo.jpeg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Layne Website</p>
+        <a
+          className="App-link"
+          href="https://laynethompson.dev"
+          //href="https://layne.website"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GOTO Site
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
